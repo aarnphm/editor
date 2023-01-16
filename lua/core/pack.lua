d21a441a53ed6177007a4f47a421f651e1764bc6
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-01-16 02:48:55 -0800

    feat; add snippets
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- lua/core/pack.lua
+++ lua/core/pack.lua
@@ -65,9 +65,11 @@ local required_plugins = function(use)
     "catppuccin/nvim",
     as = "catppuccin",
     config = function()
+      local transparent_background = false -- Set background transparency here!
+
       require("catppuccin").setup({
         flavour = "mocha", -- Can be one of: latte, frappe, macchiato, mocha
-        background = { light = "latte", dark = "frappe" },
+        background = { light = "latte", dark = "mocha" },
         dim_inactive = {
           enabled = false,
           -- Dim inactive splits/windows/buffers.
@@ -75,7 +77,8 @@ local required_plugins = function(use)
           shade = "dark",
           percentage = 0.15,
         },
-        transparent_background = false,
+        transparent_background = transparent_background,
+        show_end_of_buffer = false, -- show the '~' characters after the end of buffers
         term_colors = true,
         compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
         styles = {
@@ -109,40 +112,46 @@ local required_plugins = function(use)
               information = { "underline" },
             },
           },
-          lsp_trouble = true,
-          lsp_saga = true,
+          aerial = false,
+          barbar = false,
+          beacon = false,
+          cmp = true,
+          coc_nvim = false,
+          dap = { enabled = true, enable_ui = true },
+          dashboard = false,
+          fern = false,
+          fidget = true,
           gitgutter = false,
           gitsigns = true,
-          telescope = true,
-          nvimtree = true,
-          which_key = true,
-          indent_blankline = { enabled = true, colored_indent_levels = false },
-          dashboard = true,
-          neogit = false,
-          mason = true,
-          vim_sneak = false,
-          fern = false,
-          barbar = false,
-          markdown = true,
-          lightspeed = false,
-          ts_rainbow = true,
+          harpoon = false,
           hop = true,
           illuminate = true,
-          cmp = true,
-          dap = { enabled = true, enable_ui = true },
-          notify = false,
-          symbols_outline = false,
-          coc_nvim = false,
+          indent_blankline = { enabled = true, colored_indent_levels = false },
           leap = false,
-          neotree = { enabled = false },
-          telekasten = false,
+          lightspeed = false,
+          lsp_saga = true,
+          lsp_trouble = true,
+          markdown = true,
+          mason = true,
           mini = false,
-          aerial = false,
-          vimwiki = true,
-          beacon = false,
-          navic = false,
+          navic = { enabled = false },
+          neogit = false,
+          neotest = false,
+          neotree = { enabled = false, show_root = true, transparent_panel = false },
+          noice = false,
+          notify = true,
+          nvimtree = true,
           overseer = false,
-          fidget = true,
+          pounce = false,
+          semantic_tokens = false,
+          symbols_outline = false,
+          telekasten = false,
+          telescope = true,
+          treesitter_context = false,
+          ts_rainbow = true,
+          vim_sneak = false,
+          vimwiki = false,
+          which_key = true,
         },
         color_overrides = {
           mocha = {
@@ -176,12 +185,23 @@ local required_plugins = function(use)
           },
         },
         highlight_overrides = {
+          all = function(cp)
+            return {
+              -- For lspsaga.nvim
+              SagaBeacon = { bg = cp.surface0 },
+            }
+          end,
           mocha = function(cp)
             return {
               -- For base configs.
+              NormalFloat = { fg = cp.text, bg = transparent_background and cp.none or cp.base },
               CursorLineNr = { fg = cp.green },
               Search = { bg = cp.surface1, fg = cp.pink, style = { "bold" } },
               IncSearch = { bg = cp.pink, fg = cp.surface1 },
+              Keyword = { fg = cp.pink },
+              Type = { fg = cp.blue },
+              Typedef = { fg = cp.yellow },
+              StorageClass = { fg = cp.red, style = { "italic" } },
 
               -- For native lsp configs.
               DiagnosticVirtualTextError = { bg = cp.none },
@@ -199,35 +219,39 @@ local required_plugins = function(use)
               FidgetTask = { bg = cp.none, fg = cp.surface2 },
               FidgetTitle = { fg = cp.blue, style = { "bold" } },
 
+              -- For trouble.nvim
+              TroubleNormal = { bg = cp.base },
+
               -- For treesitter.
               ["@field"] = { fg = cp.rosewater },
               ["@property"] = { fg = cp.yellow },
 
               ["@include"] = { fg = cp.teal },
-              ["@operator"] = { fg = cp.sky },
+              -- ["@operator"] = { fg = cp.sky },
               ["@keyword.operator"] = { fg = cp.sky },
               ["@punctuation.special"] = { fg = cp.maroon },
 
-              ["@float"] = { fg = cp.peach },
-              ["@number"] = { fg = cp.peach },
-              ["@boolean"] = { fg = cp.peach },
+              -- ["@float"] = { fg = cp.peach },
+              -- ["@number"] = { fg = cp.peach },
+              -- ["@boolean"] = { fg = cp.peach },
 
               ["@constructor"] = { fg = cp.lavender },
-              ["@constant"] = { fg = cp.peach },
-              ["@conditional"] = { fg = cp.mauve },
-              ["@repeat"] = { fg = cp.mauve },
+              -- ["@constant"] = { fg = cp.peach },
+              -- ["@conditional"] = { fg = cp.mauve },
+              -- ["@repeat"] = { fg = cp.mauve },
               ["@exception"] = { fg = cp.peach },
 
               ["@constant.builtin"] = { fg = cp.lavender },
               -- ["@function.builtin"] = { fg = cp.peach, style = { "italic" } },
               -- ["@type.builtin"] = { fg = cp.yellow, style = { "italic" } },
+              ["@type.qualifier"] = { link = "@keyword" },
               ["@variable.builtin"] = { fg = cp.red, style = { "italic" } },
 
               -- ["@function"] = { fg = cp.blue },
               ["@function.macro"] = { fg = cp.red, style = {} },
               ["@parameter"] = { fg = cp.rosewater },
+              ["@keyword"] = { fg = cp.red, style = { "italic" } },
               ["@keyword.function"] = { fg = cp.maroon },
-              ["@keyword"] = { fg = cp.red },
               ["@keyword.return"] = { fg = cp.pink, style = {} },
 
               -- ["@text.note"] = { fg = cp.base, bg = cp.blue },
@@ -243,7 +267,7 @@ local required_plugins = function(use)
               ["@punctuation.bracket"] = { fg = cp.overlay2 },
               -- ["@string"] = { fg = cp.green },
               -- ["@string.regex"] = { fg = cp.peach },
-              -- ["@type"] = { fg = cp.yellow },
+              ["@type"] = { fg = cp.yellow },
               ["@variable"] = { fg = cp.text },
               ["@tag.attribute"] = { fg = cp.mauve, style = { "italic" } },
               ["@tag"] = { fg = cp.peach },
@@ -280,7 +304,12 @@ local required_plugins = function(use)
               ["@type.css"] = { fg = cp.lavender },
               ["@property.css"] = { fg = cp.yellow, style = { "italic" } },
 
+              ["@type.builtin.c"] = { fg = cp.yellow, style = {} },
+
               ["@property.cpp"] = { fg = cp.text },
+              ["@type.builtin.cpp"] = { fg = cp.yellow, style = {} },
+
+              ["@symbol"] = { fg = cp.flamingo },
             }
           end,
         },
