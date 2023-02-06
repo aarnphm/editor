f3196c1525bc1ab95e1e13e4f2488c1757a95bc4
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-02-06 07:51:42 -0800

    chore: manual update hash
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -82,8 +82,6 @@ local mapping = {
   ["n|<LocalLeader>ci"] = k.map_cr("Lspsaga incoming_calls"):with_noremap():with_silent(),
   ["n|<LocalLeader>co"] = k.map_cr("Lspsaga outgoing_calls"):with_noremap():with_silent(),
   -- toggleterm
-  ["t|<Esc><Esc>"] = k.map_cmd([[<C-\><C-n>]]):with_silent(), -- switch to normal mode in terminal.
-  ["t|jj"] = k.map_cmd([[<C-\><C-n>]]):with_silent(), -- switch to normal mode in terminal.
   ["n|<C-\\>"] = k.map_cr([[execute v:count . "ToggleTerm direction=horizontal"]]):with_defaults(),
   ["i|<C-\\>"] = k.map_cmd("<Esc><Cmd>ToggleTerm direction=horizontal<CR>"):with_defaults(),
   ["t|<C-\\>"] = k.map_cmd("<Esc><Cmd>ToggleTerm<CR>"):with_defaults(),
