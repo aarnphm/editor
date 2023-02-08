542ab8cbd36f47ef43345965acaefd603781f8d6
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2023-02-07 21:02:06 -0800

    chore: use default settings for mason-tool-installer
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- lua/modules/configs/completion/mason-tool-installer.lua
+++ lua/modules/configs/completion/mason-tool-installer.lua
@@ -26,17 +26,5 @@ return function()
 			"vale",
 			"vint",
 		},
-
-		-- if set to true this will check each tool for updates. If updates
-		-- are available the tool will be updated. This setting does not
-		-- affect :MasonToolsUpdate or :MasonToolsInstall.
-		-- Default: false
-		auto_update = true,
-
-		-- automatically install / update on startup. If set to false nothing
-		-- will happen on startup. You can use :MasonToolsInstall or
-		-- :MasonToolsUpdate to install tools and check for updates.
-		-- Default: true
-		run_on_start = true,
 	}
 end
