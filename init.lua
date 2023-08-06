ca061ce4144ced1c00c7990b585a786542d21b3f
Aaron <29749331+aarnphm@users.noreply.github.com>
2023-08-06 15:52:58 -0400

    chore: update window-picker to use opts
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -1089,21 +1089,20 @@ require("lazy").setup({
 			{
 				"s1n7ax/nvim-window-picker",
 				lazy = true,
-				config = function()
-					require("window-picker").setup {
-						autoselect_one = true,
-						include_current = false,
-						filter_rules = {
-							-- filter using buffer options
-							bo = {
-								-- if the file type is one of following, the window will be ignored
-								filetype = { "neo-tree", "neo-tree-popup", "notify" },
-								-- if the buffer type is one of following, the window will be ignored
-								buftype = { "terminal", "quickfix", "Scratch" },
-							},
+				opts = {
+					autoselect_one = true,
+					include_current = false,
+					filter_rules = {
+						-- filter using buffer options
+						bo = {
+							-- if the file type is one of following, the window will be ignored
+							filetype = { "neo-tree", "neo-tree-popup", "notify" },
+							-- if the buffer type is one of following, the window will be ignored
+							buftype = { "terminal", "quickfix", "Scratch" },
 						},
-					}
-				end,
+					},
+				},
+				config = function(_, opts) require("window-picker").setup(opts) end,
 			},
 		},
 		keys = {
