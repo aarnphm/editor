1e9fddf0ee55b1898a4208bb6537382ebb5e851f
aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>
2023-10-12 08:30:09 +0000

    chore: migrate setup to after for eager setup
    Signed-off-by: aarnphm-ec2-dev <29749331+aarnphm@users.noreply.github.com>

---
+++ after/plugin/bufremove.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<C-x>', function () require('mini.bufremove').delete(0, false) end, {desc = 'buf: delete'})
+vim.keymap.set('n', '<C-q>', function () require('mini.bufremove').delete(0, true) end, {desc = 'buf: force delete'})
