5dc7327b1a13184cad26fcf01dce518ae76675d8
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-29 19:38:48 -0500

    fix: treesitter textobjects query
    make predicates available before loading
    
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,26 +9,50 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      "windwp/nvim-ts-autotag",
-      "nvim-treesitter/nvim-treesitter-textobjects",
+      { "windwp/nvim-ts-autotag", event = Util.lazy_file_events, opts = {} },
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        config = function()
+          -- When in diff mode, we want to use the default
+          -- vim text objects c & C instead of the treesitter ones.
+          local move = require "nvim-treesitter.textobjects.move" ---@type table<string,fun(...)>
+          local configs = require "nvim-treesitter.configs"
+          for name, fn in pairs(move) do
+            if name:find "goto" == 1 then
+              move[name] = function(q, ...)
+                if vim.wo.diff then
+                  local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+                  for key, query in pairs(config or {}) do
+                    if q == query and key:find "[%]%[][cC]" then
+                      vim.cmd("normal! " .. key)
+                      return
+                    end
+                  end
+                end
+                return fn(q, ...)
+              end
+            end
+          end
+        end,
+      },
       {
         "nvim-treesitter/nvim-treesitter-context",
-        opts = {
-          enable = true,
-          max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
-          min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-          line_numbers = true,
-          multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-          trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-          mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-          zindex = 30,
-        },
+        opts = { max_lines = 3, mode = "cursor" },
       },
     },
     version = false, -- last release is way too old and doesn't work on Windows
     cmd = "TSUpdateSync",
     build = ":TSUpdate",
     event = Util.lazy_file_events,
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require "nvim-treesitter.query_predicates"
+    end,
     opts = {
       ensure_installed = "all",
       ignore_install = { "phpdoc" },
