26ab083d14dfe5be7add9d660c443357d94009be
Aaron <29749331+aarnphm@users.noreply.github.com>
2024-01-29 19:32:33 -0500

    chore: remove ts-autotag
    Signed-off-by: Aaron <29749331+aarnphm@users.noreply.github.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,7 +9,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      "windwp/nvim-ts-autotag",
       "nvim-treesitter/nvim-treesitter-textobjects",
       {
         "nvim-treesitter/nvim-treesitter-context",
