e98b8a9334c01906cceb87de5ba629ec8209243c
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2024-06-11 01:05:12 -0400

    chore: refactor config = true to empty opts to refect upstream
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -40,7 +40,7 @@ return {
     "folke/todo-comments.nvim",
     cmd = { "TodoTelescope" },
     event = { "BufReadPost", "BufNewFile" },
-    config = true,
+    opts = {},
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
