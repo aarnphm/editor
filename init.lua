82f2dde7d6b53427559d6c05e990af33b74a910d
Aaron Pham <contact@aarnphm.xyz>
2026-05-09 20:40:34 -0400

    chore: add back debug function
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 _G.Util = require "utils"
 _G.augroup = function(name) return vim.api.nvim_create_augroup(("simple_%s"):format(name), { clear = true }) end
-
+_G.P = function(...)
+  print(vim.inspect(...))
+  return ...
+end
 _G.hi = function(name, opts)
   opts.default = opts.default or true
   opts.force = opts.force or true
@@ -46,4 +49,71 @@ hi("CmpGhostText", { link = "Comment", default = true })
 hi("LeapBackdrop", { link = "Comment" })
 hi("LeapMatch", { fg = vim.go.background == "dark" and "white" or "black", bold = true, nocombine = true })
 
-Util.pack.setup()
+local specs = {
+  { src = "https://github.com/nuvic/flexoki-nvim.git",    name = "flexoki" },
+  { src = "https://github.com/echasnovski/mini.nvim.git", name = "mini.nvim" },
+  { src = "https://github.com/Saghen/blink.lib.git",      name = "blink.lib", lazy = true },
+  {
+    src = "https://github.com/Saghen/blink.cmp.git",
+    name = "blink.cmp",
+    version = "main",
+    lazy = true,
+    event = "InsertEnter",
+    dependencies = { "blink.lib", "friendly-snippets" },
+    build = function()
+      vim.cmd.packadd "blink.lib"
+      vim.cmd.packadd "blink.cmp"
+      require("blink.cmp").build():wait(60000)
+    end,
+  },
+  { src = "https://github.com/rafamadriz/friendly-snippets.git",    name = "friendly-snippets", lazy = true },
+  {
+    src = "https://github.com/L3MON4D3/LuaSnip.git",
+    name = "LuaSnip",
+    version = "master",
+    lazy = true,
+    build = (not jit.os:find "Windows")
+        and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+        or nil,
+    opts = function()
+      return {
+        history = true,
+        region_check_events = "InsertEnter",
+        delete_check_events = "TextChanged",
+        ft_func = function() return vim.split(vim.bo.filetype, ".", { plain = true }) end,
+        load_ft_func = require("luasnip.extras.filetype_functions").extend_load_ft {
+          markdown = { "lua", "json", "tex" },
+        },
+      }
+    end,
+    config = function(_, opts) require("luasnip").config.setup(opts) end,
+  },
+  {
+    src = "https://github.com/aarnphm/luasnip-latex-snippets.nvim.git",
+    name = "luasnip-latex-snippets.nvim",
+    lazy = true,
+    dependencies = { "LuaSnip" },
+  },
+  { src = "https://github.com/mason-org/mason.nvim.git",            name = "mason.nvim" },
+  { src = "https://github.com/stevearc/conform.nvim.git",           name = "conform.nvim" },
+  { src = "https://codeberg.org/andyg/leap.nvim.git",               name = "leap.nvim" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter.git", name = "nvim-treesitter",   version = "main" },
+  { src = "https://github.com/neovim/nvim-lspconfig.git",           name = "nvim-lspconfig" },
+  { src = "https://github.com/folke/lazydev.nvim.git",              name = "lazydev.nvim",      lazy = true },
+  { src = "https://github.com/Bekaboo/dropbar.nvim.git",            name = "dropbar.nvim" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim.git",         name = "gitsigns.nvim" },
+  {
+    src = "https://github.com/MagicDuck/grug-far.nvim.git",
+    name = "grug-far.nvim",
+    lazy = true,
+    cmd = "GrugFar",
+    opts = {
+      headerMaxWidth = 50,
+      windowCreationCommand = "botright vsplit",
+    },
+    config = function(_, opts) require("grug-far").setup(opts) end,
+  },
+}
+
+
+Util.pack.setup(specs)
--- lua/utils.lua
+++ lua/utils.lua
@@ -29,72 +29,6 @@ function M.on_load(name, fn)
   vim.schedule(function() fn(name) end)
 end
 
-local default_pack_specs = {
-  { src = "https://github.com/nuvic/flexoki-nvim.git", name = "flexoki" },
-  { src = "https://github.com/echasnovski/mini.nvim.git", name = "mini.nvim" },
-  { src = "https://github.com/Saghen/blink.lib.git", name = "blink.lib", lazy = true },
-  {
-    src = "https://github.com/Saghen/blink.cmp.git",
-    name = "blink.cmp",
-    version = "main",
-    lazy = true,
-    event = "InsertEnter",
-    dependencies = { "blink.lib", "friendly-snippets" },
-    build = function()
-      vim.cmd.packadd "blink.lib"
-      vim.cmd.packadd "blink.cmp"
-      require("blink.cmp").build():wait(60000)
-    end,
-  },
-  { src = "https://github.com/rafamadriz/friendly-snippets.git", name = "friendly-snippets", lazy = true },
-  {
-    src = "https://github.com/L3MON4D3/LuaSnip.git",
-    name = "LuaSnip",
-    version = "master",
-    lazy = true,
-    build = (not jit.os:find "Windows")
-        and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
-      or nil,
-    opts = function()
-      return {
-        history = true,
-        region_check_events = "InsertEnter",
-        delete_check_events = "TextChanged",
-        ft_func = function() return vim.split(vim.bo.filetype, ".", { plain = true }) end,
-        load_ft_func = require("luasnip.extras.filetype_functions").extend_load_ft {
-          markdown = { "lua", "json", "tex" },
-        },
-      }
-    end,
-    config = function(_, opts) require("luasnip").config.setup(opts) end,
-  },
-  {
-    src = "https://github.com/aarnphm/luasnip-latex-snippets.nvim.git",
-    name = "luasnip-latex-snippets.nvim",
-    lazy = true,
-    dependencies = { "LuaSnip" },
-  },
-  { src = "https://github.com/mason-org/mason.nvim.git", name = "mason.nvim" },
-  { src = "https://github.com/stevearc/conform.nvim.git", name = "conform.nvim" },
-  { src = "https://codeberg.org/andyg/leap.nvim.git", name = "leap.nvim" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter.git", name = "nvim-treesitter", version = "main" },
-  { src = "https://github.com/neovim/nvim-lspconfig.git", name = "nvim-lspconfig" },
-  { src = "https://github.com/folke/lazydev.nvim.git", name = "lazydev.nvim", lazy = true },
-  { src = "https://github.com/Bekaboo/dropbar.nvim.git", name = "dropbar.nvim" },
-  { src = "https://github.com/lewis6991/gitsigns.nvim.git", name = "gitsigns.nvim" },
-  {
-    src = "https://github.com/MagicDuck/grug-far.nvim.git",
-    name = "grug-far.nvim",
-    lazy = true,
-    cmd = "GrugFar",
-    opts = {
-      headerMaxWidth = 50,
-      windowCreationCommand = "botright vsplit",
-    },
-    config = function(_, opts) require("grug-far").setup(opts) end,
-  },
-}
-
 local pack = { specs = {} }
 local loaded = {}
 
@@ -191,7 +125,7 @@ function pack.setup(specs)
 
   pack.specs = {}
   loaded = {}
-  vim.pack.add(pack_specs_for_add(specs or default_pack_specs), { confirm = false, load = function() end })
+  vim.pack.add(pack_specs_for_add(specs), { confirm = false, load = function() end })
 end
 
 M.pack = pack
@@ -274,7 +208,7 @@ function M.set_default(option, value)
 end
 
 M.url_matcher =
-  "\\v\\c%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)%([&:#*@~%_\\-=?!+;/0-9a-z]+%(%([.;/?]|[.][.]+)[&:#*@~%_\\-=?!+/0-9a-z]+|:\\d+|,%(%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)@![0-9a-z]+))*|\\([&:#*@~%_\\-=?!+;/.0-9a-z]*\\)|\\[[&:#*@~%_\\-=?!+;/.0-9a-z]*\\]|\\{%([&:#*@~%_\\-=?!+;/.0-9a-z]*|\\{[&:#*@~%_\\-=?!+;/.0-9a-z]*})\\})+"
+"\\v\\c%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)%([&:#*@~%_\\-=?!+;/0-9a-z]+%(%([.;/?]|[.][.]+)[&:#*@~%_\\-=?!+/0-9a-z]+|:\\d+|,%(%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)@![0-9a-z]+))*|\\([&:#*@~%_\\-=?!+;/.0-9a-z]*\\)|\\[[&:#*@~%_\\-=?!+;/.0-9a-z]*\\]|\\{%([&:#*@~%_\\-=?!+;/.0-9a-z]*|\\{[&:#*@~%_\\-=?!+;/.0-9a-z]*})\\})+"
 
 ---@param win integer?
 function M.delete_url_match(win)
@@ -662,7 +596,7 @@ end
 function cmp.snippet_preview(snippet)
   local ok, parsed = pcall(function() return vim.lsp._snippet_grammar.parse(snippet) end)
   return ok and tostring(parsed)
-    or cmp
+      or cmp
       .snippet_replace(snippet, function(placeholder) return cmp.snippet_preview(placeholder.text) end)
       :gsub("%$0", "")
 end
@@ -697,7 +631,7 @@ function cmp.expand(snippet)
     ok = pcall(vim.snippet.expand, fixed)
 
     local msg = ok and "Failed to parse snippet,\nbut was able to fix it automatically."
-      or ("Failed to parse snippet.\n" .. err)
+        or ("Failed to parse snippet.\n" .. err)
 
     Util[ok and "warn" or "error"](
       ([[%s
